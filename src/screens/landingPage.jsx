79a83d6755942246ae3cffbdd6c4b7236191608d
ShaimaaHammad <shimaahammad1991@gmail.com>
2023-06-08 14:22:14 +0300

    styling

--- src/screens/landingPage.jsx
+++ src/screens/landingPage.jsx
@@ -7,23 +7,29 @@ const LandingPage = () => {
     <div id="image-holder" className="d-flex align-items-center">
       <div className="container">
         <div className="row justify-content-center justify-content-md-start">
-          <div className="col col-md-5 col-10" style={{ zIndex: "1" }}>
+          <div className="col col-lg-5 col-10" style={{ zIndex: "1" }}>
+            <div className='pb-4'>
             <h1 className="White my-3">Find parking in seconds</h1>
+
             <p className="White">
               Choose from millions of available spaces, or reserve your space in
               advance. Join over 10 million drivers enjoying easy parking.
             </p>
+            </div>
+            
             <input
               type="text"
               className="form-control"
               placeholder="Enter location"
               style={{ height: "75px", width: "100%", border:'1px solid #AA23B6' }}
             />
+
             <div
               className="row my-3"
             >
               <DateTimePickerValue />
             </div>
+
             <button
               className="btn"
               style={{ backgroundColor: "#AA23B6", color: "white" }}
