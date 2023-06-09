d33cc4878048452a1f72d2b43bf5d1d51b3083bf
Nader <naderabdalhamed93@gmail.com>
2023-06-10 00:55:36 +0300

    adding made easy section to our project

---
+++ src/components/Home-made-easy/MadeEasy.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.min.css";
+import "owl.carousel/dist/assets/owl.theme.default.min.css";
+import style from "./madeEasy.module.css";
+import WebFont from "webfontloader";
+import img1 from "../../assets/imgs/find.png";
+import img2 from "../../assets/imgs/park.png";
+import img3 from "../../assets/imgs/reserve.png";
+const MadeEasy = () => {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Nunito:400,700", "sans-serif"],
+      },
+    });
+  }, []);
+  return (
+    <section>
+      <h1 className={style.header}>Parking made easy</h1>
+      <OwlCarousel
+        className={`${style.carousel} owl-theme`}
+        items="3"
+        dots
+        responsive={{
+          0: {
+            items: 1,
+          },
+          700: {
+            items: 3,
+          },
+        }}
+      >
+        <div className={`${style.carousel_item} ${style.line} item`}>
+          <div className={style.pic}>
+            <img src={img1}></img>
+          </div>
+          <div>
+            <h2 className={`${style.sub_head} mt-5`}>Wherever, whenever</h2>
+            <p className={`${style.sub_par}`}>
+              Choose from millions of spaces across Egypt
+            </p>
+            <p className={`${style.sub_par}`}>
+              Find your best option for every car journey
+            </p>
+          </div>
+        </div>
+        <div className={`${style.carousel_item} item`}>
+          <div className={style.pic}>
+            <img src={img2}></img>
+          </div>
+          <div>
+            <h2 className={`${style.sub_head} mt-5`}>Peace of mind</h2>
+            <p className={`${style.sub_par}`}>
+              View information on availability, price and restrictions
+            </p>
+            <p className={`${style.sub_par}`}>
+              Reserve in advance at over 45,000+ locations
+            </p>
+          </div>
+        </div>
+        <div className={`${style.carousel_item} item`}>
+          <div className={style.pic}>
+            <img src={img3}></img>
+          </div>
+          <div>
+            <h2 className={`${style.sub_head} mt-5`}>Seamless experience</h2>
+            <p className={`${style.sub_par}`}>
+              Pay for IPark spaces via the app or website
+            </p>
+            <p className={`${style.sub_par}`}>
+              Follow easy directions and access instructions
+            </p>
+          </div>
+        </div>
+      </OwlCarousel>
+    </section>
+  );
+};
+export default MadeEasy;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Carousel from "../../components/Home-Slider/Carousel";
 import LandingPage from "../../components/Parking-search/LandingPage";
+import MadeEasy from "../../components/Home-made-easy/MadeEasy";
 const Home = () => {
   return (
     <Fragment>
       <LandingPage />
+      <MadeEasy />
       <Carousel />
     </Fragment>
   );
