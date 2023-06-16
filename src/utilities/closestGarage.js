1ccdfab45768850a66859d5fda6d107066a5023c
Nader <naderabdalhamed93@gmail.com>
2023-06-16 23:10:30 +0300

    refactor the closestGarage function to fix bug in asnycronous behaviour

--- src/utilities/closestGarage.js
+++ src/utilities/closestGarage.js
@@ -1,11 +1,11 @@
-/**
- * Get the nearest garages subject to passed origin coordinates
- * @param {number} originLong The origin longitude default is 30.075039276195568
- * @param {number} originLati The origin latitude default is 31.22181733648843
- * @param {number} distance optional and the default is 500 M
- * @returns {Array} promise with NearestGarages list
- * @author	"Nader"
- */
+// /**
+//  * Get the nearest garages subject to passed origin coordinates
+//  * @param {number} originLong The origin longitude default is 30.075039276195568
+//  * @param {number} originLati The origin latitude default is 31.22181733648843
+//  * @param {number} distance optional and the default is 500 M
+//  * @returns {Array} promise with NearestGarages list
+//  * @author	"Nader"
+//  */
 export default async function closestGarage(
   originLong = 30.075039276195568,
   originLati = 31.22181733648843,
@@ -15,46 +15,47 @@ export default async function closestGarage(
   const google = window.google;
   const service = new google.maps.DistanceMatrixService();
   const origin = new google.maps.LatLng(originLong, originLati);
-  let garages = await fetch(
-    `https://parking-system-eaece-default-rtdb.firebaseio.com/Garages.json`
-  )
-    .then((data) => {
-      return data.json();
-    })
-    .then((result) => {
-      return result;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  const travelMode = google.maps.TravelMode.DRIVING;
-  if (garages.length > 0) {
-    garages.forEach((garage) => {
-      const destination = new google.maps.LatLng(garage.lon, garage.lat);
-      const request = {
-        origins: [origin],
-        destinations: [destination],
-        travelMode: travelMode,
-        unitSystem: google.maps.UnitSystem.METRIC,
-      };
-      service.getDistanceMatrix(request, (response, status) => {
-        if (status === google.maps.DistanceMatrixStatus.OK) {
-          const distanceValue = response.rows[0].elements[0].distance.value;
-          if (distanceValue <= distance) {
-            NearestGarages.push({
-              garage: garage,
-              distance: `${distanceValue} Meter`,
-              duration: `${response.rows[0].elements[0].duration.text} in ${travelMode}`,
-            });
+
+  try {
+    const response = await fetch(
+      `https://parking-system-eaece-default-rtdb.firebaseio.com/Garages.json`
+    );
+    const garages = await response.json();
+
+    const travelMode = google.maps.TravelMode.DRIVING;
+    const requests = garages.map((garage) => {
+      return new Promise((resolve) => {
+        const destination = new google.maps.LatLng(garage.lon, garage.lat);
+        const request = {
+          origins: [origin],
+          destinations: [destination],
+          travelMode: travelMode,
+          unitSystem: google.maps.UnitSystem.METRIC,
+        };
+
+        service.getDistanceMatrix(request, (response, status) => {
+          if (status === google.maps.DistanceMatrixStatus.OK) {
+            const distanceValue = response.rows[0].elements[0].distance.value;
+            if (distanceValue <= distance) {
+              NearestGarages.push({
+                garage: garage,
+                distance: `${distanceValue} Meter`,
+                duration: `${response.rows[0].elements[0].duration.text} in ${travelMode}`,
+              });
+            }
+          } else {
+            console.error("Error:", status);
           }
-        } else {
-          console.error("Error:", status);
-        }
+          resolve(); // Resolve the promise after processing the response
+        });
       });
     });
-  } else {
-    console.log("there is no grages in database");
+
+    await Promise.all(requests);
+  } catch (error) {
+    console.log(error);
   }
+
   return NearestGarages;
 }
 
