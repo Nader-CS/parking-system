83ee1dd5d9d80963e038e5458ce3be837d396551
Nader <naderabdalhamed93@gmail.com>
2023-06-26 12:29:25 +0300

    adding enhancement to closest garge

--- src/utilities/closestGarage.js
+++ src/utilities/closestGarage.js
@@ -1,16 +1,7 @@
-// /**
-//  * Get the nearest garages subject to passed origin coordinates
-//  * @param {number} originLong The origin longitude default is 30.075039276195568
-//  * @param {number} originLati The origin latitude default is 31.22181733648843
-//  * @param {number} distance optional and the default is 500 M
-//  * @returns {Array} promise with NearestGarages list
-//  * @author	"Nader"
-//  */
 export default async function closestGarage(
-  
   originLati,
   originLong,
-  distance = 500
+  distance = 10000
 ) {
   const NearestGarages = [];
   const google = window.google;
@@ -23,11 +14,11 @@ export default async function closestGarage(
     );
     const garages = await response.json();
     const data = await garages;
-    const travelMode = google.maps.TravelMode.DRIVING;
-    // this
+    const travelMode = google.maps.TravelMode.WALKING;
+    console.log(data);
     const requests = Object.keys(data).map((key) => {
       let garage = { id: key, ...data[key] };
-      //
+
       return new Promise((resolve) => {
         const destination = new google.maps.LatLng(garage.lon, garage.lat);
         const request = {
@@ -39,40 +30,33 @@ export default async function closestGarage(
 
         service.getDistanceMatrix(request, (response, status) => {
           if (status === google.maps.DistanceMatrixStatus.OK) {
-            //
-            const distanceValue = response.rows[0].elements[0].distance
-              ? response.rows[0].elements[0].distance.value
-              : 1000;
-            //
-            if (distanceValue <= distance) {
-              NearestGarages.push({
-                garage: garage,
-                distance: `${distanceValue} Meter`,
-                duration: `${response.rows[0].elements[0].duration.text} in ${travelMode}`,
-              });
-              
+            console.log(response);
+            const distanceValue = response.rows[0].elements[0].distance?.value;
+            const durationText = response.rows[0].elements[0].duration?.text;
+
+            if (distanceValue !== undefined && durationText !== undefined) {
+              if (distanceValue <= distance) {
+                NearestGarages.push({
+                  garage: garage,
+                  distance: `${distanceValue} Meter`,
+                  duration: `${durationText} in ${travelMode}`,
+                });
+              }
+            } else {
+              console.error("Error: Distance or duration not available.");
             }
           } else {
             console.error("Error:", status);
           }
-          resolve(); // Resolve the promise after processing the response
+          resolve();
         });
       });
     });
 
     await Promise.all(requests);
   } catch (error) {
-    // console.log(error);
+    console.log(error);
   }
   console.log(`return ${NearestGarages}`);
   return NearestGarages;
 }
-
-/* how to use it */
-
-/*
-  closestGarage().then((result) => {
-    console.log(result);
-  });
-
-*/
